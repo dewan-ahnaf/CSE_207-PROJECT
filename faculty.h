1fc7df7220ed1f7597e63678ce2d77526e00e6cb
dewan-ahnaf <2023-3-60-193@std.ewubd.edu>
2025-07-24 18:02:50 +0600

    adding faculty

--- faculty.h
+++ faculty.h
@@ -1,6 +1,19 @@
 #ifdef Faculty_H
 #define Faculty_H
 
+typedef struct faculty{
+    LECTURER,
+    Senior_Lecturer,
+    Assistant_Professor,
+}
 
+typedef struct faculty{
+    char *name;
+    char *id;
+    int designation;
+
+}faculty;
+
+void add_faculty();
 
 #endif
